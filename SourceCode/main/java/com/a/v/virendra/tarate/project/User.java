4912c14167f4261b0cec85bc980a9eca7baf7c1e
Virendra-Tarate <91309440+virendra-tarate@users.noreply.github.com>
2023-04-13 22:11:12 +0530

    Update User.java

--- SourceCode/main/java/com/a/v/virendra/tarate/project/User.java
+++ SourceCode/main/java/com/a/v/virendra/tarate/project/User.java
@@ -1,3 +1,5 @@
+// This Project is Created by Team Interstellars For Solving For India Hack-a-thon by Geeks for Geeks
+// ©️ All Rights Reserved By Team Interstellars
 package com.a.v.virendra.tarate.project;
 
 public class User {
