095fd1a817dc65f426a5a0eeed697f7ba2cfdd48
Virendra-Tarate <91309440+virendra-tarate@users.noreply.github.com>
2023-08-05 01:51:42 +0530

    Update AboutApp.java

--- SourceCode/main/java/com/a/v/virendra/tarate/project/AboutApp.java
+++ SourceCode/main/java/com/a/v/virendra/tarate/project/AboutApp.java
@@ -1,3 +1,7 @@
+//Warning
+// Unauthorized use, reproduction, or distribution of this code, in whole or in part, without the explicit permission of the owner, is strictly prohibited and may result in severe legal consequences under the relevant IT Act and other applicable laws.
+// To use this code, you must first obtain written permission from the owner. For inquiries regarding licensing, collaboration, or any other use of the code, please contact virendratarte22@gmail.com.
+// Thank you for respecting the intellectual property rights of the owner.
 package com.a.v.virendra.tarate.project;
 
 import androidx.appcompat.app.AppCompatActivity;
